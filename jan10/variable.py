e8bcd92a9dda49fc489f0ac356d2c50d5db6f65f
Rajan Yadav <rajanyadavmce@gmail.com>
2024-01-11 08:02:49 +0545

    jan11 folder added

---
+++ jan10/variable.py
@@ -0,0 +1,14 @@
+# snake case is used in python
+
+first_name = "rajan"
+last_name = "yadav"
+
+print("my name is ", first_name  +"  " + last_name)
+
+print("type data tyep", type(first_name))
+
+first_vaiable=9.04
+print("type data tyep", type(first_vaiable))
+
+a=input("enter number")
+print(a)
---
+++ jan11/listDataType.py
@@ -0,0 +1,17 @@
+thislist = [1,2,3,"apple", "banana", "cherry"]
+print(thislist)
+
+print(type(thislist))
+print(thislist[0])
+print(thislist[1])
+print(thislist[4])
+
+print("--------------------------")
+
+print(len(thislist))
+print(thislist[-1])
+
+print("-------------------------")
+list1 = ["abc", 34, True, 40, "male"]
+print(list1)
+print(type(list1))
